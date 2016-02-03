342e8ca7d5c09c0ac2978bda1987ef569f039c45
Maxwell Gerber <mgerber@berkeley.edu>
2016-02-03 12:57:00 -0800

    Started docs. Started working on test framework.

---
+++ tnote/test/test_tnote.py
@@ -0,0 +1,19 @@
+"""Main test class for tnote"""
+
+import os
+import json
+import datetime
+
+import tnote
+
+class TestTnoteBase(object):
+    """Base class for all sandman test classes."""
+
+    def setup_method(self, _):
+        """TODO"""
+
+    def teardown_method(self, _):
+        """TODO"""
+
+class TestTnoteStuff(TestTnoteBase):
+    """TODO"""
